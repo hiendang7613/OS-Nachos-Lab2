26aba6b5e0a6aa803c422b1d65a2b0fff35bd88f
scriptifier18cs <scriptifier18cs@gmail.com>
2020-11-17 23:02:52 +0700

    third commit

--- nachos-3.4/code/test/ping.c
+++ nachos-3.4/code/test/ping.c
@@ -1,8 +1,8 @@
 #include "syscall.h"
 
 void main(){
-	int j;
-	for(j = 0; j< 10; j++)
+	int i;
+	for(i = 0; i< 1000; i++)
 	{
 		PrintConsole("A");
 	}
--- nachos-3.4/code/test/pong.c
+++ nachos-3.4/code/test/pong.c
@@ -2,7 +2,7 @@
 
 void main(){
 	int i;
-	for(i = 0; i< 10; i++)
+	for(i = 0; i< 1000; i++)
 	{
 		PrintConsole("B");
 	}
